0b10d7104aceccdc956e6b0cffadb4f0c014fcfa
Rhys Henson <rhysh@live.co.uk>
2019-03-01 11:58:48 +0000

    Time now auto updates (Trying the JS stuff I knew was a mistake)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -16,28 +17,45 @@ export class HomeComponent implements OnInit {
   lon = '0.00';
   units = 'auto';
   summery = 'current';
-  currentTime: number;
+  _currentTime: number;
+  timeInterval;
+  timeObserInterval = interval(5000);
+  TestText = 'Testing';
+
+  public get CurrentTime() {
+    return this._currentTime;
+  }
+  public set CurrentTime(_currentTime) {
+    this._currentTime = _currentTime;
+  }
 
   constructor(private http: HttpClient, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
 
-    this.apiKey = this.route.snapshot.paramMap.get("key"),
+    this.apiKey = this.route.snapshot.paramMap.get('key'),
     this.lang = this.route.snapshot.paramMap.get('lang'),
     this.lat = this.route.snapshot.paramMap.get('lat'),
     this.lon = this.route.snapshot.paramMap.get('lon'),
     this.units = this.route.snapshot.paramMap.get('units'),
     this.summery = this.route.snapshot.paramMap.get('summery');
 
-    this.currentTime = Date.now();
+    this._currentTime = Date.now();
 
 
 // tslint:disable-next-line: max-line-length
     this.weatherData = this.http.get(`https://api.darksky.net/forecast/${this.apiKey}/${this.lat},${this.lon}?units=${this.units}&lang=${this.lang}&exclude=minutely,hourly`);
-    console.log(this.weatherData);
-    console.log(this.apiKey);
-    console.log(this.route.snapshot.params);
+
+
+    this.timeObserInterval.subscribe(n => this.TimerElapse());
+  }
+
+  TimerElapse() {
+    console.log('The current time is: ' + Date.now());
+    this.CurrentTime = Date.now();
+    console.log('Current var time is: ' + this._currentTime);
+    this.TestText = 'Testing Update!';
   }
 
 }
