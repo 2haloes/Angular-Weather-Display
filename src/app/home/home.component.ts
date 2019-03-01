2fab6e4e3558d2260735834f49690858530bd110
Rhys Henson <rhysh@live.co.uk>
2019-03-01 10:11:06 +0000

    Set out routing and URL perms. Current time displays the current time on load

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  weatherData: object;
+  apiKey: string;
+  lang = 'en';
+  lat = '0.00';
+  lon = '0.00';
+  units = 'auto';
+  summery = 'current';
+  currentTime: number;
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+
+    this.apiKey = this.route.snapshot.paramMap.get("key"),
+    this.lang = this.route.snapshot.paramMap.get('lang'),
+    this.lat = this.route.snapshot.paramMap.get('lat'),
+    this.lon = this.route.snapshot.paramMap.get('lon'),
+    this.units = this.route.snapshot.paramMap.get('units'),
+    this.summery = this.route.snapshot.paramMap.get('summery');
+
+    this.currentTime = Date.now();
+
+
+// tslint:disable-next-line: max-line-length
+    this.weatherData = this.http.get(`https://api.darksky.net/forecast/${this.apiKey}/${this.lat},${this.lon}?units=${this.units}&lang=${this.lang}&exclude=minutely,hourly`);
+    console.log(this.weatherData);
+    console.log(this.apiKey);
+    console.log(this.route.snapshot.params);
   }
 
 }
