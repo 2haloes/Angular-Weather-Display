1aae14b241edea11ce270075479ac321274f415f
Rhys Henson <rhysh@live.co.uk>
2019-03-03 07:53:01 +0000

    Added weather service and setup for getting http data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { WeatherServiceService } from '../weather-service.service';
 import { HttpClient } from '@angular/common/http';
 import { interval } from 'rxjs';
 
@@ -18,9 +19,8 @@ export class HomeComponent implements OnInit {
   units = 'auto';
   summery = 'current';
   _currentTime: number;
-  timeInterval;
   timeObserInterval = interval(5000);
-  TestText = 'Testing';
+  apiURL: string;
 
   public get CurrentTime() {
     return this._currentTime;
@@ -29,7 +29,7 @@ export class HomeComponent implements OnInit {
     this._currentTime = _currentTime;
   }
 
-  constructor(private http: HttpClient, private route: ActivatedRoute) {
+  constructor(private weatherService: WeatherServiceService, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
@@ -40,22 +40,15 @@ export class HomeComponent implements OnInit {
     this.lon = this.route.snapshot.paramMap.get('lon'),
     this.units = this.route.snapshot.paramMap.get('units'),
     this.summery = this.route.snapshot.paramMap.get('summery');
-
-    this._currentTime = Date.now();
-
-
-// tslint:disable-next-line: max-line-length
-    this.weatherData = this.http.get(`https://api.darksky.net/forecast/${this.apiKey}/${this.lat},${this.lon}?units=${this.units}&lang=${this.lang}&exclude=minutely,hourly`);
-
-
+    this.apiURL = `https://api.darksky.net/forecast/${this.apiKey}/${this.lat},${this.lon}?${this.weatherService.setOptionalString(this.lang, this.units)}exclude=minutely,hourly`
+    this.CurrentTime = Date.now();
+    console.log(this.apiURL);
+    console.log(`The current unit is ${this.units} and the current lang is ${this.lang}`);
     this.timeObserInterval.subscribe(n => this.TimerElapse());
   }
 
   TimerElapse() {
-    console.log('The current time is: ' + Date.now());
     this.CurrentTime = Date.now();
-    console.log('Current var time is: ' + this._currentTime);
-    this.TestText = 'Testing Update!';
   }
 
 }
---
+++ src/app/weather-service.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  dataGet(pageUrl: string){
+    return this.http.get(pageUrl);
+  }
+
+  setOptionalString(lang: string, units: string){
+    let returnString = '';
+
+    if (!!lang && this.genLangList().includes(lang)) {
+      returnString += `?lang=${lang}&`;
+    }
+
+    if (!!units && this.genUnitList().includes(units)) {
+      returnString += `?units=${units}&`;
+    }
+
+    return returnString;
+  }
+
+  genLangList(){
+    return [
+      'ar',
+      'az',
+      'be',
+      'bg',
+      'bs',
+      'ca',
+      'cs',
+      'da',
+      'de',
+      'el',
+      'en', // The default
+      'es',
+      'et',
+      'fi',
+      'fr',
+      'he',
+      'hr',
+      'hu',
+      'id',
+      'is',
+      'it',
+      'ja',
+      'ka',
+      'ko',
+      'kw',
+      'lv',
+      'nb',
+      'nl',
+      'no',
+      'pl',
+      'pt',
+      'ro',
+      'ru',
+      'sk',
+      'sl',
+      'sr',
+      'sv',
+      'tet',
+      'tr',
+      'uk',
+      'x-pig-latin',
+      'zh',
+      'zh-tw'
+    ];
+  }
+
+  genUnitList() {
+    return [
+      'auto',
+      'ca',
+      'uk2',
+      'us', // The default
+      'si'
+    ]
+  }
+
+}
