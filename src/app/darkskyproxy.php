2db18c9ba846ba2fe44d1dbbc7d6790f796ad83f
Rhys Henson <rhysh@live.co.uk>
2019-03-19 12:01:39 +0000

    PHP rewritten, fixed day display issue caused when removing extra divs

--- src/app/darkskyproxy.php
+++ src/app/darkskyproxy.php
@@ -1,13 +1,27 @@
 <?
+$errorReturn = '{"currently": {"icon": "API-ERROR"}}';
+
+if (extension_loaded('curl')) {
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_TIMEOUT, 1);
   curl_setopt($ch, CURLOPT_URL, str_replace('_', '&', $_GET['api']));
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
   if (FALSE === ($retval = curl_exec($ch))) {
     error_log(curl_error($ch));
+    printf($errorReturn);
   } else {
     printf($retval);
   }
-  // $outdata = file_get_contents($_GET['api']);
-  // printf("{Test: 'Hello!'}");
+} elseif (condition) {
+  $retval = file_get_contents(str_replace('_', '&', $_GET['api']));
+  if ($retval !== false) {
+    printf($retval);
+  } else {
+    error_log('file_get_contents: Error retriving data from DarkSky API');
+    printf($errorReturn);
+  }
+} else {
+  error_log('Angular Weather Display: Cannot find compatible API download method');
+  printf($errorReturn);
+}
 ?>
